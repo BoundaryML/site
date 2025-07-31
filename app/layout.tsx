b2ac309faaaf5b8f75411eac86ef24dfad47556b
Chris Watts <chris.watts.t@gmail.com>
2025-07-31 14:22:38 -0700

    feat: add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,9 +9,20 @@ import type { Viewport } from 'next';
 import { Suspense } from 'react';
 import { AnalyticsProvider } from '@/context/analytics';
 
+const baseUrl =
+  process.env.NEXT_PUBLIC_BASE_URL ??
+  (process.env.VERCEL_PROJECT_PRODUCTION_URL
+    ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
+    : 'http://localhost:3000');
 export const metadata: Metadata = {
+  alternates: {
+    canonical: `${baseUrl}/`,
+  },
   description:
     'BAML is a tool for developers to build AI applications with type safety and reliability',
+  icons: {
+    icon: '/favico.ico',
+  },
   metadataBase: new URL(
     process.env.VERCEL_ENV === 'production'
       ? 'https://boundaryml.com'
@@ -20,15 +31,20 @@ export const metadata: Metadata = {
   openGraph: {
     description:
       'BAML is a tool for developers to build AI applications with type safety and reliability',
+    locale: 'en_US',
     siteName: 'BAML',
     title: 'BAML',
+    type: 'website',
     url: 'https://boundaryml.com',
   },
   title: 'BAML',
   twitter: {
     card: 'summary_large_image',
     creator: '@boundaryml',
+    description:
+      'Boundary makes it easy to build, test, and develop LLM applications.',
     site: '@boundaryml',
+    title: 'Boundary | The best way to get structured data with LLMs',
   },
 };
 
