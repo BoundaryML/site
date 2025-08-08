d00a36a516b4306aa467d8f60bcb100e168dafa4
hellovai <vbv@boundaryml.com>
2025-08-08 01:14:09 -0700

    fix posthog

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -3,4 +3,6 @@ import { env } from '@/lib/env';
 
 posthog.init(env.NEXT_PUBLIC_POSTHOG_KEY, {
   api_host: env.NEXT_PUBLIC_POSTHOG_HOST,
+  debug: process.env.NODE_ENV === 'development',
+  defaults: '2025-05-24',
 });
